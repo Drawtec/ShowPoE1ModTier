fb6cf782002a27616246e50dd4728a0a24c832cb
Drawtec <darkxell133@gmail.com>
2025-04-10 23:58:49 +0200

    Update ShowPoE1ModTierSettings.cs
    Added a Switch to turn on or off the StashItemModScanner and added Color Nodes for the CharColor

--- ShowPoE1ModTierSettings.cs
+++ ShowPoE1ModTierSettings.cs
@@ -1,5 +1,9 @@
+using ExileCore2.Shared.Attributes;
 using ExileCore2.Shared.Interfaces;
 using ExileCore2.Shared.Nodes;
+
+
+
 
 namespace ShowPoE1ModTier;
 
@@ -8,10 +12,26 @@ public class ShowPoE1ModTierSettings : ISettings
     public ShowPoE1ModTierSettings()
     {
         Enable = new ToggleNode(false);
-        Debug = new ToggleNode(true);
+
     }
 
-    private ToggleNode Debug { get; set; }
+
     public ToggleNode Enable { get; set; }
+    
+    public SMTSettings Features { get; set; } = new SMTSettings();
+
+    [Submenu(CollapsedByDefault = false)]
+    public class SMTSettings
+    {
+        [Menu("Switch between PoE1 and PoE2 ModTier Style")]
+        public ToggleNode PoEModSwitch { get; set; } = new ToggleNode(true);
+
+        [Menu("Scan Stash and show good Tier icons on items")]
+        public ToggleNode InventoryScanSwitch { get; set; } = new ToggleNode(false);
+        [Menu("set the Color for T1 Mod")]
+        public ColorNode Tier1Mod { get; set; } = new ColorNode(4294902015);
+        [Menu("set the Color for T2 Mod")]
+        public ColorNode Tier2Mod { get; set; } = new ColorNode(16711935);
+    }
 }
 
