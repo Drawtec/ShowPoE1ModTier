e1f050e1b76734229a5a42bcb03d236593587cc5
Drawtec <darkxell133@gmail.com>
2025-04-11 00:00:42 +0200

    Update ITier.cs
    added PoE2 ModTier Style could be switched in the settings

--- Classes/ITier.cs
+++ Classes/ITier.cs
@@ -1,4 +1,4 @@
-﻿using ExileCore2.PoEMemory.MemoryObjects;
+using ExileCore2.PoEMemory.MemoryObjects;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
@@ -6,32 +6,42 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
+
 namespace ShowPoE1ModTier.Classes
 {
     class ITier
     {
 
-        public static string ItemTier(Entity pEntity,ItemMod iMod, JObject jAffixeType)
+        public static string ItemTier(ItemMod iMod, JObject jAffixeType, bool tierMode)
         {
 
             var modRecord = iMod.ModRecord;
-            var modName = iMod.Name;
             var modGroup = modRecord.TypeName;
             var rawModName = iMod.RawName;
+
             int rawModTier = Int32.Parse(Regex.Match(rawModName, @"\d+").Value);      
             
             var jModGroup = jAffixeType.GetValue(modGroup);
             var imodGroup = jModGroup.ToList<JToken>();
             var poeModTier = (imodGroup.Count + 1) - rawModTier;
-            var result = modGroup + "  T" + poeModTier.ToString();
-            return result;
+            var maxTiers = imodGroup.Count;
+            string result;
+            if (tierMode)
+            {
+                result = modGroup + "  T" + poeModTier.ToString();
+            }
+            else 
+            {
+                result = modGroup + "  T" + rawModTier.ToString() + " / " + maxTiers.ToString();
+            }
+
+                return result;
         }
         public static JObject ModList()
         {
 
 
             string json = @"Plugins/Temp/ShowPoE1ModTier/json/mods.json";
-            var jsonFile = File.ReadAllText(json);
             if(!File.Exists(json)) json = @"Plugins/Compiled/ShowPoE1ModTier/json/mods.json";
             if (File.Exists(json))
             {
